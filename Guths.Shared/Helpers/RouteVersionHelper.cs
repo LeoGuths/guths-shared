bfa648ad6aae9754d4add57e6262198931ccfd0e
Leonardo Güths <leonardoguths@gmail.com>
2025-07-16 11:08:03 -0300

    chore: add helper for more dynamic version control

---
+++ Guths.Shared/Helpers/RouteVersionHelper.cs
@@ -0,0 +1,7 @@
+namespace Guths.Shared.Helpers;
+
+public static class RouteVersionHelper
+{
+    public static string BuildRoute(string version, string route) =>
+        $"{version}/{route.TrimStart('/')}";
+}
--- Guths.Shared/Web/Versioning/EApiVersions.cs
+++
@@ -1,7 +0,0 @@
-namespace Guths.Shared.Web.Versioning;
-
-public enum EApiVersions
-{
-    V1,
-    V2
-}
